ed521b532f3aab96116d68bb769e862a1c8bba5a
Clay <dev@cegan.dev>
2024-12-09 15:10:25 -0700

    Minor optimizations for Day9

--- day9/day9.go
+++ day9/day9.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Disk []int
@@ -17,10 +18,18 @@ type Spaces map[int]int
 
 func main() {
 	input := readInput("input.txt")
+
+	start := time.Now()
 	disk := parseDisk(input)
-	part1(disk)
+	part1(&disk)
+	duration := time.Since(start)
+	fmt.Printf("Part 1 execution time: %v\n", duration)
+
+	start = time.Now()
 	disk = parseDisk(input)
-	part2(disk)
+	part2(&disk)
+	duration = time.Since(start)
+	fmt.Printf("Part 2 execution time: %v\n", duration)
 }
 
 func readInput(file string) string {
@@ -78,26 +87,24 @@ func strToInt(s string) int {
 	return val
 }
 
-func part1(disk Disk) {
-	compacted := compactDisk(disk)
-	checksum := calculateChecksum(&compacted)
+func part1(disk *Disk) {
+	compactDisk(disk)
+	checksum := calculateChecksum(disk)
 	fmt.Printf("The checksum after compaction is %d\n", checksum)
 }
 
-func part2(disk Disk) {
-	defraged := defragDisk(disk)
-	checksum := calculateChecksum(&defraged)
+func part2(disk *Disk) {
+	defragDisk(disk)
+	checksum := calculateChecksum(disk)
 	fmt.Printf("The checksum after defragging is %d\n", checksum)
 }
 
-func compactDisk(disk Disk) Disk {
-	for spaceAvailable(&disk) {
-		a := getLastFileBlockIndex(&disk)
-		b := getFirstEmptyBlockIndex(&disk)
-		disk[a], disk[b] = disk[b], disk[a]
+func compactDisk(disk *Disk) {
+	for spaceAvailable(disk) {
+		a := getLastFileBlockIndex(disk)
+		b := getFirstEmptyBlockIndex(disk)
+		(*disk)[a], (*disk)[b] = (*disk)[b], (*disk)[a]
 	}
-
-	return disk
 }
 
 func spaceAvailable(disk *Disk) bool {
@@ -119,7 +126,6 @@ func getLastFileBlockIndex(disk *Disk) int {
 			return i
 		}
 	}
-
 	panic("No file blocks found!")
 }
 
@@ -129,7 +135,6 @@ func getFirstEmptyBlockIndex(disk *Disk) int {
 			return i
 		}
 	}
-
 	panic("No empty blocks found!")
 }
 
@@ -144,96 +149,62 @@ func calculateChecksum(disk *Disk) int {
 	return sum
 }
 
-func defragDisk(disk Disk) Disk {
-	files := analyzeFiles(&disk)
-	spaces := analyzeSpaces(&disk)
+func defragDisk(disk *Disk) {
+	files, spaces := analyzeDisk(disk)
 
 	for i := len(files) - 1; i > 0; i-- {
 		file := files[i]
-		space_idx := getFirstAvailableSpaceIdx(&spaces, file.length)
+		space_idx := getFirstAvailableSpaceIdx(spaces, file.length)
 		if space_idx == -1 || space_idx > file.startIdx {
 			continue
 		}
 		moveFile(disk, file, space_idx)
-		spaces = analyzeSpaces(&disk)
+		spaces = updateSpaces(spaces, space_idx, file.length)
 
 	}
 
-	return disk
 }
 
-func analyzeFiles(disk *Disk) Files {
+func analyzeDisk(disk *Disk) (Files, Spaces) {
 	files := make(Files, 0)
-
-	counts := make(map[int]int, 0)
-	for _, v := range *disk {
-		if v != -1 {
-			counts[v]++
-		}
-	}
-
-	for id, count := range counts {
-		idx := getFileStartIdx(disk, id)
-		files = append(files, File{id, count, idx})
-	}
-
-	return sortFiles(files)
-}
-
-func sortFiles(files Files) Files {
-	for range files {
-		swapped := false
-		for j := 0; j < len(files)-1; j++ {
-			if files[j].id > files[j+1].id {
-				files[j], files[j+1] = files[j+1], files[j]
-				swapped = true
-			}
-		}
-		if !swapped {
-			break
-		}
-	}
-
-	return files
-}
-
-func getFileStartIdx(disk *Disk, id int) int {
-	for i, v := range *disk {
-		if v == id {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func analyzeSpaces(disk *Disk) Spaces {
 	spaces := make(Spaces, 0)
-	startIdx := -1
-	count := 0
+	spaceIdx := -1
 
-	for i, v := range *disk {
-		if v == -1 {
-			if startIdx == -1 {
-				startIdx = i
+	for i, id := range *disk {
+		if id == -1 {
+			if spaceIdx == -1 {
+				spaceIdx = i
 			}
-			count++
+			spaces[spaceIdx]++
 		} else {
-			if startIdx == -1 {
-				continue
+			if spaceIdx != -1 {
+				spaceIdx = -1
 			}
-			spaces[startIdx] = count
-			startIdx = -1
-			count = 0
+			if len(files) < id+1 {
+				files = append(files, File{id, 0, i})
+			}
+			files[id].length++
 		}
+	}
+
+	return files, spaces
+}
+
+func updateSpaces(spaces Spaces, idx, file_size int) Spaces {
+	if (spaces)[idx] == file_size {
+		delete((spaces), idx)
+	} else {
+		size := (spaces)[idx] - file_size
+		delete((spaces), idx)
+		(spaces)[idx+file_size] = size
 	}
 
 	return spaces
 }
 
-func getFirstAvailableSpaceIdx(spaces *Spaces, size int) int {
+func getFirstAvailableSpaceIdx(spaces Spaces, size int) int {
 	minIdx := -1
-	for idx, count := range *spaces {
+	for idx, count := range spaces {
 		if count >= size {
 			if minIdx == -1 {
 				minIdx = idx
@@ -246,10 +217,8 @@ func getFirstAvailableSpaceIdx(spaces *Spaces, size int) int {
 	return minIdx
 }
 
-func moveFile(disk Disk, file File, idx int) {
+func moveFile(disk *Disk, file File, idx int) {
 	for i := 0; i < file.length; i++ {
-		if file.id == 7 {
-		}
-		disk[idx+i], disk[file.startIdx+i] = disk[file.startIdx+i], disk[idx+i]
+		(*disk)[idx+i], (*disk)[file.startIdx+i] = (*disk)[file.startIdx+i], (*disk)[idx+i]
 	}
 }
