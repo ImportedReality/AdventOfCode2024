d1d4c7f295488126c5d7411c0d7b48a5ac41ed3d
Clay <dev@cegan.dev>
2024-12-13 12:32:02 -0700

    Day 12 part 2 complete

--- day12/day12.go
+++ day12/day12.go
@@ -22,15 +22,20 @@ var grid aocutils.Grid[string]
 var garden aocutils.Grid[Plant]
 var regions []Region
 
+const N, NE, E, SE, S, SW, W, NW = 0, 1, 2, 3, 4, 5, 6, 7
+
 func main() {
 	grid = aocutils.ReadGrid(filename, "")
 	readGarden()
 	findRegions()
 	total := 0
+	bulkTotal := 0
 	for _, region := range regions {
 		total += calculateRegionPrice(region)
+		bulkTotal += calculateBulkPrice(region)
 	}
 	fmt.Printf("Total price: %d\n", total)
+	fmt.Printf("Total bulk price: %d\n", bulkTotal)
 }
 
 func readGarden() {
@@ -134,4 +139,53 @@ func calculateRegionPrice(region Region) int {
 		perimeter += 4 - len(plant.neighbours)
 	}
 	return area * perimeter
+}
+
+func calculateBulkPrice(region Region) int {
+	area := len(region)
+	sides := 0
+	for _, plant := range region {
+		neighbourInfo := analyzeNeighbours(plant)
+		sides += numSides(neighbourInfo)
+	}
+	return area * sides
+}
+
+func analyzeNeighbours(plant Plant) map[aocutils.Direction]bool {
+	neighbours := make(map[aocutils.Direction]bool)
+	for dir := aocutils.N; dir <= aocutils.NW; dir++ {
+		offset := aocutils.Offsets[dir]
+		x, y := plant.loc.X+offset.X, plant.loc.Y+offset.Y
+		toCheck := aocutils.Coordinate{X: x, Y: y}
+		if aocutils.InBounds(garden, toCheck) {
+			neighbours[dir] = garden[toCheck.Y][toCheck.X].plantType == plant.plantType
+		}
+	}
+	return neighbours
+}
+
+func numSides(neighbours map[aocutils.Direction]bool) int {
+	count := 0
+	if !neighbours[N] {
+		if !(neighbours[E] && !neighbours[NE]) {
+			count++
+		}
+	}
+	if !neighbours[E] {
+		if !(neighbours[S] && !neighbours[SE]) {
+			count++
+		}
+	}
+	if !neighbours[S] {
+		if !(neighbours[W] && !neighbours[SW]) {
+			count++
+		}
+	}
+	if !neighbours[W] {
+		if !(neighbours[N] && !neighbours[NW]) {
+			count++
+		}
+	}
+
+	return count
 }
